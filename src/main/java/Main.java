40ead982941d27338f52b5fa055aabd3659c4096
Paltinych <pgumennov@yandex.ru>
2023-02-11 16:10:00 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,58 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static final String REMOTE_SERVICE_URI = "https://api.nasa.gov/planetary/apod?api_key=kavb5pc4PzqtOfsnzVCXuzTywg0CcfrvEmOJjphf";
+    public static ObjectMapper mapper = new ObjectMapper();
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        Post post = mapper.readValue(
+                response.getEntity().getContent(),
+                Post.class
+        );
+        String url = post.getHdurl();
+        System.out.println(url);
+
+        String[] s = url.split("/");
+        System.out.println(s[s.length - 1]);
+        File jpeg = new File(s[s.length - 1]);
+
+        HttpGet hdurl = new HttpGet(post.getHdurl());
+        hdurl.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        response = httpClient.execute(hdurl);
+
+        try (FileOutputStream fos = new FileOutputStream(jpeg)) {
+            fos.write(response.getEntity().getContent().readAllBytes());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+
+        response.close();
+        httpClient.close();
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,48 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+    private final String copyright;
+    private final String date;
+    private final String explanation;
+    private final String hdurl;
+    private final String mediaType;
+    private final String serviceVersion;
+    private final String title;
+    private final String url;
+    public Post(
+            @JsonProperty("copyright") String copyright,
+            @JsonProperty("date") String date,
+            @JsonProperty("explanation") String explanation,
+            @JsonProperty("hdurl") String hdurl,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
+            @JsonProperty("title") String title,
+            @JsonProperty("url") String url
+    ) {
+        this.copyright = copyright;
+        this.date = date;
+        this.explanation = explanation;
+        this.hdurl = hdurl;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" +
+                "\n  copyright : " + copyright+
+                "\n  date : " + date +
+                "\n  explanation : " + explanation +
+                "\n  hdurl : " + hdurl +
+                "\n  media-type : " + mediaType +
+                "\n  service-version : " + serviceVersion +
+                "\n  title : " + title +
+                "\n  url : " + url;
+    }
+}
